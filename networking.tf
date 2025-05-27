de25d7fd6632cb863ddc11cfcb00092401e538cb
Lakshya Gulenia <lakshyagulenia451@gmail.com>
2025-05-27 18:01:42 +0530

    changes in networking file and outputfile

--- networking.tf
+++ networking.tf
@@ -5,3 +5,12 @@ tags = {
 Name = "student-vpc"
  }
 }
+resource "aws_subnet" "subnet-1" {
+    vpc_id = aws_vpc.main-vpc.id
+    cidr_block = "10.1.0.0/16"
+    map_public_ip_on_launch = "true"
+    tags = {
+      Name = "student-subnet-1"
+    }
+  
+}
