1ed27b581f3ee0f9392bd498de38109fb010d276
Lakshya Gulenia <lakshyagulenia451@gmail.com>
2025-05-28 12:43:45 +0530

    chnages in networking file and added variables

--- networking.tf
+++ networking.tf
@@ -7,7 +7,7 @@ Name = "student-vpc"
 }
 resource "aws_subnet" "subnet-1" {
     vpc_id = aws_vpc.main-vpc.id
-    cidr_block = "10.1.0.0/16"
+    cidr_block = var.subnet-1_cidr_block
     map_public_ip_on_launch = "true"
     tags = {
       Name = "student-subnet-1"
